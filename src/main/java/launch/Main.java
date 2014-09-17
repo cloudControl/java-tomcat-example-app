7b2b38bdc4be8fd4dd016ad5a6efb0c72d4d5ef8
Dimitris Verraros <vervas.g@gmail.com>
2014-09-17 14:13:39 +0200

    cloudControl adaptions

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -5,23 +5,13 @@ import org.apache.catalina.startup.Tomcat;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        String port = System.getenv("PORT") != null ? System.getenv("PORT") : "8080";
 
-        String webappDirLocation = "src/main/webapp/";
         Tomcat tomcat = new Tomcat();
-
-        //The port that we should run on can be set into an environment variable
-        //Look for that variable and default to 8080 if it isn't there.
-        String webPort = System.getenv("PORT");
-        if(webPort == null || webPort.isEmpty()) {
-            webPort = "8080";
-        }
-
-        tomcat.setPort(Integer.valueOf(webPort));
-
-        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
-        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+        tomcat.setPort(Integer.valueOf(port));
+        tomcat.addWebapp("/", new File("src/main/webapp/").getAbsolutePath());
 
         tomcat.start();
-        tomcat.getServer().await();  
+        tomcat.getServer().await();
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,14 @@
+<!doctype html>
 <html>
+<head>
+    <link href='/static/helloworld.css' rel='stylesheet' type='text/css'/>
+    <title>Hello World in Tomcat</title>
+</head>
 <body>
-<h2>Hello Heroku!</h2>
+    <strong>Hello world, I'm a Java app running on Tomcat</strong>
+    <footer>
+        Coming from the <a href="https://www.cloudcontrol.com/dev-center/Quickstart">Quickstart</a>?
+        Check out <a href="https://www.cloudcontrol.com/dev-center/Guides/Java/Java%20-%20Tomcat">the explanation</a>.
+    </footer>
 </body>
 </html>
